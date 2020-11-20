7cf75209783013d7638446dbdf7ce983b1983b59
Jacob <jrammer101@gmail.com>
2020-11-19 19:13:16 -0800

    sf

--- labs/lab6/lab6.c
+++ labs/lab6/lab6.c
@@ -17,10 +17,10 @@ typedef struct
 typedef struct
 {
     char name[MAXNAME];
-    MealTicket* const buffer;
+    MealTicket* buffer;
     int head;
     int tail;
-    const int length;
+    int length;
 }MTQ;
 
 MTQ *registry[MAXQUEUES];
@@ -32,7 +32,7 @@ int isFull(MTQ* queue)
     Return 1 if full, else 0
     */
 
-   return queue->length == ENTRIES;
+   return queue->length >= ENTRIES;
 }
 
 int isEmpty(MTQ* queue)
@@ -60,21 +60,28 @@ int enqueue(char* MTQ_ID, MealTicket* MT)
         if(strcmp(MTQ_ID, registry[i]->name) == 0)
         {
             registryIndex = i;
-            printf("Index is: %d\n", registryIndex);
             break;
         }
     }
 
-    if(registryIndex == -1)
+    if(registryIndex == -1)  // Queue name not found
         return 0;
     if(!isFull(registry[registryIndex]))
     {
-        printf("Queue is not full");
+        registry[registryIndex]->buffer[registry[registryIndex]->head] = *MT;
+        printf("Head is: %d\n", registry[registryIndex]->head);
+        printf("pushing: Queue: <%s> - Ticket Number: <%d> - Dish: <%s>\n",
+               registry[registryIndex]->name, registry[registryIndex]->buffer[registry[registryIndex]->head].ticketNum,
+               registry[registryIndex]->buffer[registry[registryIndex]->head].dishName);
+        registry[registryIndex]->head++;
+        registry[registryIndex]->head = registry[registryIndex]->head % ENTRIES;
+        registry[registryIndex]->length++;
+        // printf("Head is: %d\n", registry[registryIndex]->head);
+    }else  // queue is full
+    {
+        printf("%s is full. Nothing queued\n", registry[registryIndex]->name);
+        return 0;
     }
-
-
-
-    int retVal = 0;
 }
 
 int main()
@@ -83,20 +90,24 @@ int main()
     MTQ* Lunch;
     MTQ* Dinner;
     MTQ* Bar;
-    MealTicket* breakfastTicket;
-    MealTicket* lunchTicket;
-    MealTicket* dinnerTicket;
-    MealTicket* barTicket;
-    breakfastTicket =  malloc(sizeof(MealTicket));
-    lunchTicket = malloc(sizeof(MealTicket));
-    dinnerTicket = malloc(sizeof(MealTicket));
-    barTicket = malloc(sizeof(MealTicket));
+    MealTicket breakfastTicket;
+    MealTicket lunchTicket;
+    MealTicket dinnerTicket;
+    MealTicket barTicket;
+    // breakfastTicket =  malloc(sizeof(MealTicket));
+    // lunchTicket = malloc(sizeof(MealTicket));
+    // dinnerTicket = malloc(sizeof(MealTicket));
+    // barTicket = malloc(sizeof(MealTicket));
 
 
     Breakfast = malloc(sizeof(MTQ));
     Lunch = malloc(sizeof(MTQ));
     Dinner = malloc(sizeof(MTQ));
     Bar = malloc(sizeof(MTQ));
+    Breakfast->buffer = malloc(sizeof(MealTicket) * 3);
+    Lunch->buffer = malloc(sizeof(MealTicket) * 3);
+    Dinner->buffer = malloc(sizeof(MealTicket) * 3);
+    Bar->buffer = malloc(sizeof(MealTicket) * 3);
 
     Breakfast->head = 0;
     Breakfast->tail = 0;
@@ -107,17 +118,17 @@ int main()
     Bar->head = 0;
     Bar->tail = 0;
 
-    breakfastTicket->dishName = "Eggs";
-    breakfastTicket->ticketNum = 0;
+    breakfastTicket.dishName = "Eggs";
+    breakfastTicket.ticketNum = 0;
 
-    lunchTicket->dishName = "Turkey Sandwhich";
-    lunchTicket->ticketNum = 1;
+    lunchTicket.dishName = "Turkey Sandwhich";
+    lunchTicket.ticketNum = 1;
 
-    dinnerTicket->dishName = "Steak";
-    dinnerTicket->ticketNum = 2;
+    dinnerTicket.dishName = "Steak";
+    dinnerTicket.ticketNum = 2;
 
-    barTicket->dishName = "Beer";
-    barTicket->ticketNum = 3;
+    barTicket.dishName = "Beer";
+    barTicket.ticketNum = 3;
 
     registry[0] = Breakfast;
     registry[1] = Lunch;
@@ -127,11 +138,15 @@ int main()
     strcpy(Lunch->name, "Lunch");
     strcpy(Dinner->name, "Dinner");
     strcpy(Bar->name, "Bar");
-    
 
     for(int i = 0; i < 4; i++)
         printf("Registry[%d] name is: %s\n", i, registry[i]->name);
-    // enqueue("Lunch", breakfastTicket);
+    enqueue("Breakfast", &breakfastTicket);
+    enqueue("Breakfast", &breakfastTicket);
+    enqueue("Breakfast", &breakfastTicket);
+    enqueue("Breakfast", &breakfastTicket);
+    enqueue("Breakfast", &breakfastTicket);
+    // printf("First entry: %s\n", registry[0]->buffer[registry[0]->head].dishName);
 
     return 0;
 }
