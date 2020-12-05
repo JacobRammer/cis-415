3272309c0d192efe36cb5d9a3f0b784dcf39bf13
Jacob <jrammer101@gmail.com>
2020-12-05 15:46:01 -0800

    commit

--- Projects/Project3/quacker.c
+++ Projects/Project3/quacker.c
@@ -83,7 +83,7 @@ void initTopic(Topic* t, int id)
 void initRegistry()
 {
     for (int i = 0; i < MAXQUEUES; i++)
-        registry[i]->topicID = -1;
+        registry[i].topicID = -1;
 }
 
 void initQueue(Queue* q, char* qName, int topicType)
@@ -118,7 +118,7 @@ int findQueue(int topicType)
     int regIndex = -1;
     for (int i = 0; i < MAXQUEUES; i++)
     {
-        if (registry[i]->topicID == topicType)
+        if (registry[i].topicID == topicType)
         {
             regIndex = i;
             break;
@@ -138,23 +138,23 @@ int enqueue(int topicType, Topic* t)
         printf("From enqueue: Queue type not found\n");
         return 0;
     }    
-    if(!isFull(registry[regIndex]))
+    if(!isFull(&registry[regIndex]))
     {
-        pthread_mutex_lock(&registry[regIndex]->mutex);
-        t->entryNum = registry[regIndex]->totalItems;
-        registry[regIndex]->buffer[registry[regIndex]->head] = *t;
-        registry[regIndex]->head++;
-        registry[regIndex]->head = registry[regIndex]->head % MAXTICKETS;
-        registry[regIndex]->length++;
-        registry[regIndex]->totalItems++;
+        pthread_mutex_lock(&registry[regIndex].mutex);
+        t->entryNum = registry[regIndex].totalItems;
+        registry[regIndex].buffer[registry[regIndex].head] = *t;
+        registry[regIndex].head++;
+        registry[regIndex].head = registry[regIndex].head % MAXTICKETS;
+        registry[regIndex].length++;
+        registry[regIndex].totalItems++;
         printf("From enqueue: Successfully pushed <%d> into queue <%d>\n",
-               t->entryNum, registry[regIndex]->topicID);
-        pthread_mutex_unlock(&registry[regIndex]->mutex);
+               t->entryNum, registry[regIndex].topicID);
+        pthread_mutex_unlock(&registry[regIndex].mutex);
         return 1;
     }else
     {
         printf("From enqueue: Pushing: Queue: <%s> - Error queue is full.\n", 
-            registry[regIndex]->name);
+            registry[regIndex].name);
         return 0;
     }
     return 0;    
@@ -169,26 +169,26 @@ int dequeue(int topicID, Topic* t)
         printf("From dequeue: queue type not found\n");
         return 0;
     }
-    if(!isEmpty(registry[regIndex]))
+    if(!isEmpty(&registry[regIndex]))
     {
-        pthread_mutex_lock(&registry[regIndex]->mutex);
-        memcpy(t, &registry[regIndex]->buffer[registry[regIndex]->tail], sizeof(Topic));
-        registry[regIndex]->tail++;
-        registry[regIndex]->tail = registry[regIndex]->tail % MAXTICKETS;
-        registry[regIndex]->length--;
-        registry[regIndex]->totalItems++;
+        pthread_mutex_lock(&registry[regIndex].mutex);
+        memcpy(t, &registry[regIndex].buffer[registry[regIndex].tail], sizeof(Topic));
+        registry[regIndex].tail++;
+        registry[regIndex].tail = registry[regIndex].tail % MAXTICKETS;
+        registry[regIndex].length--;
+        registry[regIndex].totalItems++;
         printf("From dequeue: Successfully popped <%d> from queue <%d>\n", 
-        t->entryNum, registry[regIndex]->topicID);
-        registry[regIndex]->lastRead = t->entryNum;
+        t->entryNum, registry[regIndex].topicID);
+        registry[regIndex].lastRead = t->entryNum;
         printf("EntryNum %d\n", t->entryNum);
-        printf("Last read: %d\n", registry[regIndex]->lastRead);
-            pthread_mutex_unlock(&registry[regIndex]->mutex);
+        printf("Last read: %d\n", registry[regIndex].lastRead);
+            pthread_mutex_unlock(&registry[regIndex].mutex);
         return 1;
     }
     else
     {
         printf("From dequeue: popping: Queue: <%s> - Error queue is empty.\n",
-               registry[regIndex]->name);
+               registry[regIndex].name);
         return 0;
     }
 
@@ -203,31 +203,31 @@ int getEntry(int lastEntry, int topicType, Topic* t)
     and is the entryNum for that topic. 
     */
     int location = findQueue(topicType);
-    int max = registry[location]->length;
-    int itter = registry[location]->max;
-    // printf("Head: %d\n", registry[location]->le);
+    int max = registry[location].length;
+    int itter = registry[location].max;
+    // printf("Head: %d\n", registry[location].le);
     printf("Location %d\n", location);
-    pthread_mutex_lock(&registry[location]->mutex);
+    pthread_mutex_lock(&registry[location].mutex);
 
     // Case 1
-    if(isEmpty(registry[location]))
+    if(isEmpty(&registry[location]))
         return 0;
 
     // Case 2
     // printf("Max: %d\n\n", itter);   
-    int tail = registry[location]->tail; 
+    int tail = registry[location].tail; 
     for(int i = tail; i < itter; i++)
     {
-        // printf("From entry: last read %d\n", registry[location]->lastRead);
+        // printf("From entry: last read %d\n", registry[location].lastRead);
         // printf("From entry: looking for tickerNum (lastread + 1): %d\n", lastEntry + 1);
-        // printf("Test 3: %d\n", registry[location]->buffer[i].entryNum);
-        int test3 = registry[location]->buffer[i].entryNum;
+        // printf("Test 3: %d\n", registry[location].buffer[i].entryNum);
+        int test3 = registry[location].buffer[i].entryNum;
         int test4 = lastEntry + 1;
-        if(registry[location]->buffer[i].entryNum == lastEntry + 1) 
+        if(registry[location].buffer[i].entryNum == lastEntry + 1) 
             {
                 printf("Case 2\n");
-                memcpy(t, &registry[location]->buffer[i], sizeof(Topic));
-                pthread_mutex_unlock(&registry[location]->mutex);
+                memcpy(t, &registry[location].buffer[i], sizeof(Topic));
+                pthread_mutex_unlock(&registry[location].mutex);
                 return 1;
             }
             // printf("\n\n");
@@ -238,31 +238,31 @@ int getEntry(int lastEntry, int topicType, Topic* t)
     int ltFlag = 0;
     for (int i = tail; i < itter; i++)
     {
-        int test = registry[location]->buffer[i].entryNum;
+        int test = registry[location].buffer[i].entryNum;
         int test2 = lastEntry + 1;
-        if (registry[location]->buffer[i].entryNum <= lastEntry + 1)
+        if (registry[location].buffer[i].entryNum <= lastEntry + 1)
         {
             ltFlag++;
             if(ltFlag == max)
             {
                 printf("Case 3a\n");
-                // memcpy(t, &registry[location]->buffer[i], sizeof(Topic));
-                pthread_mutex_unlock(&registry[location]->mutex);
+                // memcpy(t, &registry[location].buffer[i], sizeof(Topic));
+                pthread_mutex_unlock(&registry[location].mutex);
                 return 0;
             }
         }
     }
 
     // case 3b
-    registry[location]->buffer[registry[location]->head].entryNum = 100;
-    if(registry[location]->buffer[registry[location]->head].entryNum > lastEntry + 1)
+    registry[location].buffer[registry[location].head].entryNum = 100;
+    if(registry[location].buffer[registry[location].head].entryNum > lastEntry + 1)
     {
         printf("Case 3b\n");
-        memcpy(t, &registry[location]->buffer[location], sizeof(Topic));
+        memcpy(t, &registry[location].buffer[location], sizeof(Topic));
         return 1;
     }
 
-    pthread_mutex_unlock(&registry[location]->mutex);
+    pthread_mutex_unlock(&registry[location].mutex);
     return 0;
 }
 
@@ -275,26 +275,26 @@ void* cleanUp()
         Topic *t1;
         t1 = malloc(sizeof(Topic));
 
-        int found = findQueue(registry[i]->topicID);
+        int found = findQueue(registry[i].topicID);
         printf("found: %d\n", found);
         if(found != -1)
         { 
-            int head = registry[i]->head;
-            int tail = registry[i]->length;
+            int head = registry[i].head;
+            int tail = registry[i].length;
             for(int j = head; j != tail; j++)  // TODO fix this. think this is good
             {
-                char* name = registry[found]->buffer[j].photoCaption;
+                char* name = registry[found].buffer[j].photoCaption;
                 // sleep(5);
-                gettimeofday(&registry[found]->buffer[j].cleanTime, NULL);
-                long test = registry[found]->buffer[j].cleanTime.tv_sec - registry[found]->buffer[j].startTime.tv_sec;
+                gettimeofday(&registry[found].buffer[j].cleanTime, NULL);
+                long test = registry[found].buffer[j].cleanTime.tv_sec - registry[found].buffer[j].startTime.tv_sec;
                 printf("Tail: %d, head: %d\n", tail, head);
-                printf("Cleanup: %s\n", registry[found]->buffer[j].photoCaption);
+                printf("Cleanup: %s\n", registry[found].buffer[j].photoCaption);
                 printf("Time test: %ld\n", test);
                 if(test > delta)
                 {
                     printf("Cleanup: old entry found. Removing ticket: %d %s\n",
-                           registry[found]->buffer[j].entryNum, registry[found]->buffer[j].photoCaption);
-                    dequeue(registry[found]->topicID, t1);
+                           registry[found].buffer[j].entryNum, registry[found].buffer[j].photoCaption);
+                    dequeue(registry[found].topicID, t1);
                 }
             }
         }
@@ -385,16 +385,140 @@ int numQueues(FILE* file)
             numTopics++;
         }
     }
+    free(lineBuffer);
     return returnVal;
+}
+
+int numPublishers(FILE* file)
+{
+    /*
+    Gets the number of publishers from the input file. 
+    This can be denoted as "add publisher""
+    */
+
+    int publisher = 0;
+    int bufferSize = 256;
+    char* lineBuffer;
+    char* savePtr;
+    lineBuffer = (char*)malloc(sizeof(char) * bufferSize);
+    while(fgets(lineBuffer, bufferSize, file) != NULL)
+    {
+        char* temp = strtok_r(lineBuffer, " ", &savePtr);
+        temp = strtok_r(NULL, " ", &savePtr);
+        printf("Temp: %s\n", temp);
+        if(strcmp(temp, "publisher") == 0)
+        {
+            publisher++;
+        }
+    }
+    free(lineBuffer);
+    return publisher;
+}
+
+void findDelta(FILE* file)
+{
+    /*
+    Goes through the file and finds the delta time
+    */
+
+    int bufferSize = 256;
+    char* lineBuffer;
+    char* savePtr;
+    lineBuffer = (char*)malloc(sizeof(char) * bufferSize);
+    while(fgets(lineBuffer, bufferSize, file) != NULL)
+    {
+        char* temp = strtok_r(lineBuffer, " ", &savePtr);
+        if(strcmp(temp, "delta") == 0)
+        {
+            temp = strtok_r(NULL, " ", &savePtr);
+            delta = atoi(temp);
+            printf("Delta is: %d\n", delta);
+        }
+    }
+    free(lineBuffer);
+}
+
+void addPubArray(FILE* file, char** array)
+{
+    /*
+    Add publishers and subscribers to a array so we
+    can open up those files later
+    */
+   int bufferSize = 256;
+   char* lineBuffer;
+   char* savePtr;
+   int index = 0;
+   lineBuffer = (char*)malloc(sizeof(char) * bufferSize);
+   while(fgets(lineBuffer, bufferSize, file) != NULL)
+   {
+       char* temp = strtok_r(lineBuffer, " ", &savePtr);
+       temp = strtok_r(NULL, " ", &savePtr);
+       if(strcmp(temp, "publisher") == 0) 
+       {
+           temp = strtok_r(NULL, " ", &savePtr);
+           int length = strlen(temp);
+           char newFileName[length];
+           memset(newFileName, '\0', sizeof(char) * length);
+           for(int i = 0; i < length; i++)
+           {
+                char tem = temp[i];   
+               if(temp[i] != '"' && temp[i] != '\n')
+               {
+                   strncat(newFileName, &temp[i], 1);
+               } 
+           }
+        //    printf("Temp is: %s\n", temp);
+            printf("Filename is: %s\n", newFileName);
+            strcpy(array[index], newFileName);
+            index++;
+       }
+   }
+   free(lineBuffer);
+}
+
+void addSubArray(FILE* file, char** array)
+{
+    int bufferSize = 256;
+    char *lineBuffer;
+    char *savePtr;
+    int index = 0;
+    lineBuffer = (char *)malloc(sizeof(char) * bufferSize);
+    while (fgets(lineBuffer, bufferSize, file) != NULL)
+    {
+        char *temp = strtok_r(lineBuffer, " ", &savePtr);
+        temp = strtok_r(NULL, " ", &savePtr);
+        if (strcmp(temp, "subscriber") == 0)
+        {
+            temp = strtok_r(NULL, " ", &savePtr);
+            int length = strlen(temp);
+            char newFileName[length];
+            memset(newFileName, '\0', sizeof(char) * length);
+            for (int i = 0; i < length; i++)
+            {
+                char tem = temp[i];
+                if (temp[i] != '"' && temp[i] != '\n')
+                {
+                    strncat(newFileName, &temp[i], 1);
+                }
+            }
+            //    printf("Temp is: %s\n", temp);
+            printf("Filename is: %s\n", newFileName);
+            strcpy(array[index], newFileName);
+            index++;
+        }
+    }
+    free(lineBuffer);
 }
 
 void testQueue()
 {
+    // numTopics = 3;
+    // registry = (Queue *)malloc(sizeof(Queue) * numTopics);
     printf("*** Testing queue data structure ***\n");
     Queue *q;
     q = malloc(sizeof(Queue));
     initQueue(q, "Test Queue", 1);
-    registry[0] = q;
+    registry[0] = *q;
 
     Topic *t1;
     t1 = malloc(sizeof(Topic));
@@ -425,8 +549,8 @@ void testQueue()
     for (int i = 0; i < MAXTICKETS; i++)
     {
         printf("From main testing enqueue: publisher <%d> caption %s\n",
-               registry[0]->buffer[i].pubID, registry[0]->buffer[i].photoCaption);
-        // printf("Test entryNum: %d\n", registry[0]->buffer[i].entryNum);
+               registry[0].buffer[i].pubID, registry[0].buffer[i].photoCaption);
+        // printf("Test entryNum: %d\n", registry[0].buffer[i].entryNum);
     }
 
     // Topic *t4;
@@ -450,7 +574,7 @@ void testEntry()
     Queue *q;
     q = malloc(sizeof(Queue));
     initQueue(q, "Test Queue", 1);
-    registry[0] = q;
+    registry[0] = *q;
 
     Topic *t1;
     t1 = malloc(sizeof(Topic));
@@ -483,21 +607,21 @@ void testEntry()
     enqueue(1, t1);
     Topic *t4;
     t4 = malloc(sizeof(Topic));
-    printf("Main test: %d\n", registry[0]->lastRead);
+    printf("Main test: %d\n", registry[0].lastRead);
     
     dequeue(1, t4);  // last read is 0
 
     Topic* t5;
     t5 = malloc(sizeof(Topic));
-    printf("From main: last read %d\n", registry[0]->lastRead);
-    getEntry(registry[0]->lastRead, 1, t5);  // last entry should be 0
+    printf("From main: last read %d\n", registry[0].lastRead);
+    getEntry(registry[0].lastRead, 1, t5);  // last entry should be 0
     printf("Testing lastEntry %s\n", t5->photoCaption);
     dequeue(1, t4);
-    getEntry(registry[0]->lastRead, 1, t5); // last entry should be 0
+    getEntry(registry[0].lastRead, 1, t5); // last entry should be 0
     printf("Testing lastEntry %s\n", t5->photoCaption);
-    registry[0]->lastRead = -2;
-    printf("FML: %d\n", registry[0]->lastRead);
-    getEntry(registry[0]->lastRead, 1, t5);
+    registry[0].lastRead = -2;
+    printf("FML: %d\n", registry[0].lastRead);
+    getEntry(registry[0].lastRead, 1, t5);
     printf("After modified last readEntry: Testing lastEntry %s\n", t5->photoCaption);
     // dequeue(1, t4); // last read is 1
     // dequeue(1, t4);  // last read is 2
@@ -509,7 +633,7 @@ void testCleanup()
     Queue *q;
     q = malloc(sizeof(Queue));
     initQueue(q, "Test Queue", 1);
-    registry[0] = q;
+    registry[0] = *q;
 
     Topic *t1;
     t1 = malloc(sizeof(Topic));
@@ -535,12 +659,12 @@ void testCleanup()
     Queue* q2;
     q2 = malloc(sizeof(Queue));
     initQueue(q2, "T2 Queue", 2);
-    registry[1] = q2;
+    registry[1] = *q2;
 
     enqueue(1, t1);
     enqueue(1, t2);
     enqueue(2, t3);
-    printf("Test: %s\n", registry[1]->buffer[0].photoCaption);
+    printf("Test: %s\n", registry[1].buffer[0].photoCaption);
     // dequeue(1, t5);
     cleanUp();
 }
@@ -550,12 +674,12 @@ void testPubSub()
     Queue *q;
     q = malloc(sizeof(Queue));
     initQueue(q, "Test Queue", 1);
-    registry[0] = q;
+    registry[0] = *q;
 
     Queue *q2;
     q2 = malloc(sizeof(Queue));
     initQueue(q2, "Test Queue", 2);
-    registry[1] = q2;
+    registry[1] = *q2;
 
     Topic *t1;
     t1 = malloc(sizeof(Topic));
@@ -623,17 +747,17 @@ void testPubSub()
     pthread_create(&subThread[0], NULL, subscriber, &globalSubs[0]);
     sleep(1);
     pthread_cond_broadcast(&globalCond);
-    printf("Registry test: %s, %s, %s\n", registry[0]->buffer[0].photoCaption,
-           registry[0]->buffer[1].photoCaption, registry[0]->buffer[2].photoCaption);
+    printf("Registry test: %s, %s, %s\n", registry[0].buffer[0].photoCaption,
+           registry[0].buffer[1].photoCaption, registry[0].buffer[2].photoCaption);
     pthread_join(pubThread[0], NULL);
     pthread_join(clean, NULL);
     pthread_join(subThread[0], NULL);
     // getEntry(0, 1, t5);
-    // printf("Testing entry again: %s\n", t5->photoCaption);
-    // // registry[0]->buffer[1].entryNum = 25;
+    // printf("Testing entry again: %s\n", t5.photoCaption);
+    // // registry[0].buffer[1].entryNum = 25;
     // dequeue(1, t5);
     // getEntry(2, 1, t5);
-    // printf("Testing entry again: %s\n", t5->photoCaption);
+    // printf("Testing entry again: %s\n", t5.photoCaption);
     // sleep(2);
     // cleanUp();
     
@@ -653,6 +777,7 @@ int main(int argc, char* argv[])
 
     FILE* fName;
     int numCommands;
+    int numPubs;
 
     fName = fopen(argv[1], "r");
     if(fName == NULL)
@@ -664,6 +789,30 @@ int main(int argc, char* argv[])
     numCommands = numQueues(fName);
     printf("Num of queues needed: %d\n", numCommands);
     registry = (Queue*)malloc(sizeof(Queue) * numTopics);
+    fName = fopen(argv[1], "r");
+    numPubs = numPublishers(fName);
+    fName = fopen(argv[1], "r");
+    findDelta(fName);
+    printf("Num pubs: %d\n", numPubs);
+    fName = fopen(argv[1], "r");
+    char* temp;
+    char *publisherArray[numPubs];
+    char* subscriberArray[numPubs];
+    for(int i = 0; i < numPubs; i++)
+    {
+        publisherArray[i] = (char *)malloc(sizeof(char) & 256);
+        subscriberArray[i] = (char *)malloc(sizeof(char) & 256);
+    }
+    addPubArray(fName, publisherArray);
+    fName = fopen(argv[1], "r");
+    addSubArray(fName, subscriberArray);
+
+    
+    // publisherArray[0] = "Test";
+    // publisherArray[1] = "Test1";
+    // publisherArray[2] = "Test2";
+    printf("Final test: %s\n", publisherArray[0]);
+    printf("Final test: %s\n", subscriberArray[0]);
 
     return 0;
 }
